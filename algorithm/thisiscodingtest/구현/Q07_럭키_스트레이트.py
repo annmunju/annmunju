5154d5857f24503accecc34be7b937388bbddd90
mungdo <82261307+mungdo@users.noreply.github.com>
2022-03-20 14:33:13 +0900

    folder

--- algorithm/thisiscodingtest/구현/Q07_럭키_스트레이트.py
+++
@@ -1,8 +0,0 @@
-num_ls = list(map(int, list(input())))
-
-bif = len(num_ls)//2
-
-if (sum(num_ls[:bif]) == sum(num_ls[bif:])):
-    print('LUCKY')
-else:
-    print('READY')
--- algorithm/thisiscodingtest/그리디/난수 생성.py
+++
@@ -1,7 +0,0 @@
-from random import randint
-
-
-for n in range(1000):
-    num = randint(0, 10)
-    print(num, end=' ')
-
